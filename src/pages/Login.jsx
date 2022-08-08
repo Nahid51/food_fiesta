750dac073659fd450955775b650b1d5e9fc7e7b9
Nahid51 <hasannahidnahid@gmail.com>
2022-08-08 16:25:46 +0600

    added user password login system

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/commonSection/CommonSection';
-import { Container, Row, Col } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import login from '../assets/images/login.jpg';
-import '../styles/login.css';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import loginPhoto from '../assets/images/login.jpg';
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { login } from '../store/authentication/userSlice';
+
+const initialState = {
+    email: "",
+    password: ""
+}
 
 const Login = () => {
+    const [formValue, setFormValue] = useState(initialState);
+    const { loading, error } = useSelector((state) => ({ ...state.auth }));
+    const { email, password } = formValue;
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        error && toast.error(error);
+    }, [error]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (email && password) {
+            dispatch(login({ formValue, navigate, toast }));
+        }
+    };
+    const onInputChange = (e) => {
+        let { name, value } = e.target;
+        setFormValue({ ...formValue, [name]: value });
+    };
+
+    // const googleSuccess = (resp) => {
+    //   const email = resp?.profileObj?.email;
+    //   const name = resp?.profileObj?.name;
+    //   const token = resp?.tokenId;
+    //   const googleId = resp?.googleId;
+    //   const result = { email, name, token, googleId };
+    //   dispatch(googleSignIn({ result, navigate, toast }));
+    // };
+    // const googleFailure = (error) => {
+    //   toast.error(error);
+    // };
 
     return (
         <Helmet title="Login">
@@ -15,35 +54,54 @@ const Login = () => {
                 <Container>
                     <Row>
                         <Col lg="6" md="6" sm="12" className='text-center my-auto'>
-                            <form className="form mb-5">
-                                <div className="form_group">
-                                    <input type="email" placeholder='Email' required />
-                                </div>
+                            <Form onSubmit={handleSubmit} className="mb-5 mx-3 p-3 rounded-1" style={{ background: "#FDE4E4" }}>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Control
+                                        className='border-top-0 border-start-0 border-end-0 rounded-0 bg-transparent'
+                                        type="email"
+                                        value={email}
+                                        name="email"
+                                        onChange={onInputChange}
+                                        placeholder="Enter email"
+                                        required
+                                    />
+                                </Form.Group>
 
-                                <div className="form_group">
-                                    <input type="password" placeholder='Password' required />
-                                </div>
-                                <button type='submit' className="addToCart_btn">Login</button>
-                            </form>
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Control
+                                        className='border-top-0 border-start-0 border-end-0 rounded-0 bg-transparent'
+                                        type="password"
+                                        value={password}
+                                        name="password"
+                                        onChange={onInputChange}
+                                        placeholder="Password"
+                                        required
+                                    />
+                                </Form.Group>
 
-                            <NavLink to="/register" style={{ color: "#212245" }}>Don't have an account? Create an account</NavLink>
+                                <Button style={{ fontSize: 14 }} variant="danger" type="submit">
+                                    Login
+                                </Button>
+                            </Form>
+
+                            <Link to="/register">Don't have an account? Sign Up</Link>
 
                             <div className='mt-5 mb-2' style={{ color: "#212245" }}>
                                 or Login with
                             </div>
 
                             <div className='d-flex align-items-center justify-content-center gap-3'>
-                                <button className='google_icon'>
+                                <Button variant="danger">
                                     <i className="ri-google-fill"></i>
-                                </button>
-                                <button className='facebook_icon'>
+                                </Button>
+                                <Button className='facebook_icon'>
                                     <i className="ri-facebook-fill"></i>
-                                </button>
+                                </Button>
                             </div>
                         </Col>
 
                         <Col lg="6" md="6" sm="12">
-                            <img src={login} alt="login_photo" className='w-100' />
+                            <img src={loginPhoto} alt="login_photo" className='w-100' />
                         </Col>
                     </Row>
                 </Container>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Helmet from '../components/Helmet/Helmet';
 import CommonSection from '../components/UI/commonSection/CommonSection';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import register from '../assets/images/register.jpg';
 
@@ -17,42 +17,52 @@ const Register = () => {
                         </Col>
 
                         <Col lg="6" md="6" sm="12" className='text-center my-auto'>
-                            <form className="form mb-5">
-                                <div className="form_group">
-                                    <input
+                            <Form className="mb-5 mx-3 p-3 rounded-1" style={{ background: "#FDE4E4" }}>
+                                <Form.Group className="mb-3" controlId="formBasicName">
+                                    <Form.Control
+                                        className='border-top-0 border-start-0 border-end-0 rounded-0 bg-transparent'
                                         type="text"
-                                        placeholder='Name'
+                                        name="name"
+                                        placeholder="Enter name"
                                         required
                                     />
-                                </div>
+                                </Form.Group>
 
-                                <div className="form_group">
-                                    <input
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Control
+                                        className='border-top-0 border-start-0 border-end-0 rounded-0 bg-transparent'
                                         type="email"
-                                        placeholder='Email'
+                                        name="email"
+                                        placeholder="Enter email"
                                         required
                                     />
-                                </div>
+                                </Form.Group>
 
-                                <div className="form_group">
-                                    <input
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Control
+                                        className='border-top-0 border-start-0 border-end-0 rounded-0 bg-transparent'
                                         type="password"
-                                        placeholder='Password'
+                                        name="password"
+                                        placeholder="Password"
                                         required
                                     />
-                                </div>
+                                </Form.Group>
 
-                                <div className="form_group">
-                                    <input
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Control
+                                        className='border-top-0 border-start-0 border-end-0 rounded-0 bg-transparent'
                                         type="password"
-                                        placeholder='Re-type password'
+                                        name="confirmPassword"
+                                        placeholder="Re-type Password"
                                         required
                                     />
-                                </div>
+                                </Form.Group>
 
-                                <button type='submit' className="addToCart_btn">Sign Up</button>
-                            </form>
-                            <Link to="/login">Already have an account? Login now</Link>
+                                <Button style={{ fontSize: 14 }} variant="danger" type="submit">
+                                    Register
+                                </Button>
+                            </Form>
+                            <Link to="/login">Already have an account? Sign In</Link>
                         </Col>
 
                     </Row>
